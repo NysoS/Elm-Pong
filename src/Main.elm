852c9bfaa7b063d1d446a3c4b454057be5a411b6
NysoS <nwanysos79@gmail.com>
2022-12-04 16:56:16 +0100

    Draw screen & ball

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,33 @@
 module Main exposing (..)
 
-import Html
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+type alias Ball =
+    { x : Int
+    , y : Int
+    }
+
+ball : Ball
+ball = 
+    { x = 250
+    , y = 250
+    }
 
 main =
-    Html.text "Hello World"
+    svg [ width "500"
+        , height "500"
+        , viewBox "0 0 500 500"
+        , Svg.Attributes.style "background: #efefef"
+        ]
+        [ viewBall ball
+        ]
+
+viewBall : Ball -> Svg msg 
+viewBall { x, y } =
+    circle 
+        [ cx <| String.fromInt x
+        , cy <| String.fromInt y
+        , r "10"
+        ]
+        [] 
