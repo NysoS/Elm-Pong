b885e06b83dde32b63d7ff397e8d636954592684
NysoS <nwanysos79@gmail.com>
2022-12-04 17:18:21 +0100

    add runtime deltatime

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,7 @@
 module Main exposing (..)
 
+import Browser
+import Browser.Events exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
@@ -8,22 +10,58 @@ type alias Ball =
     , y : Int
     }
 
-ball : Ball
-ball = 
-    { x = 250
-    , y = 250
-    }
+type alias Model =
+    Ball
 
+
+type Msg
+    = OnAnimationFrame Float
+
+
+type alias Flags = 
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( { x = 250
+      , y = 250
+      }
+    , Cmd.none
+    )
+
+
+main : Program Flags Model Msg
 main =
-    svg [ width "500"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        OnAnimationFrame deltaTime ->
+            ( {model | x = model.x + 4 }, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Browser.Events.onAnimationFrameDelta OnAnimationFrame
+
+view : Model -> Svg.Svg Msg
+view model =
+     svg [ width "500"
         , height "500"
         , viewBox "0 0 500 500"
         , Svg.Attributes.style "background: #efefef"
         ]
-        [ viewBall ball
+        [ viewBall model
         ]
 
-viewBall : Ball -> Svg msg 
+
+viewBall : Ball -> Svg Msg 
 viewBall { x, y } =
     circle 
         [ cx <| String.fromInt x
